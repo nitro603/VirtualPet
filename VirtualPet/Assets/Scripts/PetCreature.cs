5219b6aae33354d20c5449efe24bf24043424296
nitro603 <tavaracristian93@gmail.com>
2026-02-06 13:50:10 -0500

    Update PetCreature.cs

--- VirtualPet/Assets/Scripts/PetCreature.cs
+++ VirtualPet/Assets/Scripts/PetCreature.cs
@@ -2,50 +2,56 @@ using UnityEngine;
 
 public class PetCreature : MonoBehaviour
 {
-    public int currentHungry;
-    public int currentCleanliness;
-    public int currentBoredom;
-    //private int MaxValue = 100;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int _currentHungry;
+    private int _currentCleanliness;
+    private int _currentBoredom;
+ 
     void Start()
     {
-        currentHungry = 100;
-        currentCleanliness = 100;
-        currentBoredom = 100;
-    }
-    
-    void Update()
-    {
-        //Debug.Log(currentHungry / MaxValue);
+        _currentHungry = 100;
+        _currentCleanliness = 100;
+        _currentBoredom = 100;
     }
 
     public void SubtractCare(int tick)
     {
         Debug.Log(tick);
-        currentHungry -= 2;
+        _currentHungry -= 2;
         switch (tick)
         {
             case 2: case 4:
-                currentCleanliness -= 5;
+                _currentCleanliness -= 5;
                 break;
-         
             case 5:
-                currentBoredom -= 25;
+                _currentBoredom -= 25;
                 break;
         }
     }
 
     public int GetHunger()
     {
-        return currentHungry;
+        return _currentHungry;
     }
     public int GetCleanliness()
     {
-        return currentCleanliness;
+        return _currentCleanliness;
     }
     public int GetBoredom()
     {
-        return currentBoredom;
+        return _currentBoredom;
     }
+
+    public void RecoverHungerStatus()
+    {
+        _currentHungry += 2;
+    }
+    public void RecoverCleanlinessStatus()
+    {
+        _currentCleanliness += 2;
+    }
+    public void RecoverBoredomStatus()
+    {
+        _currentBoredom += 2;
+    }
+    
 }
