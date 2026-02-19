aacb8298b231a33f11e535d499bb8e9f53218468
nitro603 <tavaracristian93@gmail.com>
2026-02-19 16:36:03 -0500

    Setup Animation State Changes

--- VirtualPet/Assets/Scripts/PetCreature.cs
+++ VirtualPet/Assets/Scripts/PetCreature.cs
@@ -1,16 +1,52 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PetCreature : MonoBehaviour
 {
+    private Animator _animator;
+    
     private int _currentHungry;
     private int _currentCleanliness;
     private int _currentBoredom;
- 
+    
+
     void Start()
     {
         _currentHungry = 100;
         _currentCleanliness = 100;
         _currentBoredom = 100;
+
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (_currentHungry < 30)
+        {
+            _animator.SetBool("isHungry",true);
+        }
+        if (_currentCleanliness < 40)
+        {
+            _animator.SetBool("isUnclean",true);
+        }
+        if (_currentBoredom < 50)
+        {
+            _animator.SetBool("isBored",true);
+        }
+        //chopped if statements 
+        if (_currentHungry > 30)
+        {
+            _animator.SetBool("isHungry",false);
+        }
+        if (_currentCleanliness > 40)
+        {
+            _animator.SetBool("isUnclean",false);
+        }
+        if (_currentBoredom > 50)
+        {
+            _animator.SetBool("isBored",false);
+        }
     }
 
     public void SubtractCare(int tick)
